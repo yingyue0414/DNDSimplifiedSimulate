bfc9f8433edc6493f5ea04ba4e095de8fd9b41ae
Yue Ying <yying7@jh.edu>
2024-08-01 13:04:36 -0400

    Delete duplicate function and add depre link

--- src/attack_roll.py
+++ src/attack_roll.py
@@ -3,7 +3,7 @@ Module providing NUMERICAL functions to make attack rolls.
 """
 
 import random
-
+import src.roll_modifier as rmod
 
 def roll_dice(num_dice):
     """
@@ -21,6 +21,8 @@ def roll_dice(num_dice):
 
 def calculate_success_and_failure(dice_rolls, str_modifier, success_threshold=4):
     """
+    This function is moved to roll_modifier.py, and I left a link here.
+    
     Calculate the number of success and failure dice based on the dice rolls and strength modifier.
 
     Args:
@@ -30,42 +32,13 @@ def calculate_success_and_failure(dice_rolls, str_modifier, success_threshold=4)
     Returns:
         tuple: A tuple containing the number of success dice, failure dice, and adjusted dice results.
     """
-    adjusted_rolls = []
 
     # DICE ROLLS MUST BE IN INCREASING ORDER!
     # Apply positive or negative modifier starting from the highest failure die
-    if str_modifier > 0:
-        for roll in reversed(dice_rolls):
-            # if this roll failed ..
-            if roll < success_threshold:
-                roll += str_modifier  # Add current str_modifier to roll
-                if roll > success_threshold:  # but if then roll exceeds success_threshold
-
-                    # Put exceeded part back ..
-                    str_modifier = roll - success_threshold
-                    roll = success_threshold  # .. and set roll to exactly success_threshold
-                else:
-                    str_modifier = 0  # Apply the modifier only once
-            adjusted_rolls.append(roll)
-        adjusted_rolls = adjusted_rolls[::-1]
-    elif str_modifier < 0:
-        for roll in dice_rolls:
-            # if this roll succeeded ..
-            if roll >= success_threshold and str_modifier < 0:
-                roll += str_modifier  # Add current str_modifier to roll
-                if roll < success_threshold - 1:  # but if then roll is lower then success_threshold - 1
-                    # Put exceeded part back ..
-                    str_modifier = roll - success_threshold + 1
-                    roll = success_threshold - 1  # .. and set roll to exactly success_threshold - 1
-                else:
-                    str_modifier = 0  # Apply the modifier only once
-            adjusted_rolls.append(roll)
-    else:
-        adjusted_rolls = dice_rolls.copy()
-
-    success_count = sum(
-        1 for roll in adjusted_rolls if roll >= success_threshold)
-    failure_count = len(dice_rolls) - success_count
+    
+    # See roll_modifier.py
+    _, adjusted_rolls, success_count, failure_count =\
+        rmod.modify(dice_rolls, str_modifier, success_threshold)
 
     return success_count, failure_count, adjusted_rolls
 
